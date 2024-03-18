32e45a976dfef551ddf0f975f47d09b6b3ca2962
Syahrezaimamw <syahrezaimamwahyudi976@gmail.com>
2024-03-18 14:41:53 +0700

    fiks margin detail project

--- src/DetailProject.jsx
+++ src/DetailProject.jsx
@@ -21,14 +21,14 @@ export const DetailProject = () => {
 
             <div className='flex'>
                 <Navbar />
-                <div className='flex flex-wrap items-start justify-center w-full md:py-4'>
+                <div className='flex flex-wrap items-start justify-center w-full mt-5 mb-5 md:mt-0 md:mb-0 md:py-4'>
                     <div className='w-[90%] flex flex-wrap justify-start gap-9 items-start'>
                     {loading ?
                         data.map((a, i) => {
                             
                                 // console.log(a)
                                 return (
-                                    <div className='w-full sm:w-[47%] md:w-full lg:w-[47%] xl:w-[31%] ' key={i}>
+                                    <div className='w-full sm:w-[47%] md:w-full lg:w-[47%]  xl:w-[31%] ' key={i}>
 
                                         <Card theme={i % 2 == 1} title={a.title} link={a.link} path='path="/Dashboard.github.io/' id={a.id} description={a.description} language={a.language} key={i} />
                                     </div>
